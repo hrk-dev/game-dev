8ef017df77295e5250ce6923f935a2c770d44204
blacktunes <blacktunes@foxmail.com>
2021-05-14 01:54:20 +0800

    perf: 重做制作组显示插件

--- js/plugins/Credits.js
+++ js/plugins/Credits.js
@@ -1,16 +1,37 @@
 /*:
- * @plugindesc 制作组
+ * @plugindesc 主页按钮
  * @author Blacktunes
  *
- * @param 公共事件ID
+ * @param 按钮标题A
+ * @default 制作组
+ *
+ * @param 按钮A公共事件ID
  * @default 1
  * @type number
  *
- * @param 标题
- * @desc 标题选项的名称用语。
- * @default 制作组
+ * @param 按钮标题B
+ * @default 寂しい
  *
+ * @param 按钮B公共事件ID
+ * @default 2
+ * @type number
+ *
+ * @param 回忆之间组按钮标题
+ * @default 回忆之间
+ *
+ * @param 回忆之间公共事件ID
+ * @default 3
+ * @type number
+ *
+ * @help
+ * SET_CREDITS Boolean 是否显示标题A(false为显示B)
+ * SET_CREDITS_NORMAL Boolean 是否显示正常的制作组
+ * SET_MEMOR Boolean 是否显示回忆之间
  */
+
+const globalInfo = DataManager.loadGlobalInfo() || [];
+globalInfo[0] = { ...{ memory: false, credits: false, normal: false }, ...globalInfo[0] };
+DataManager.saveGlobalInfo(globalInfo);
 
 var Credits = {};
 Credits.Parameters = PluginManager.parameters('Credits');
@@ -18,21 +39,78 @@ Credits.Parameters = PluginManager.parameters('Credits');
 var _makeCommandList = Window_TitleCommand.prototype.makeCommandList;
 Window_TitleCommand.prototype.makeCommandList = function () {
 	_makeCommandList.call(this);
-	this.addCommand(String(Credits.Parameters['标题'] || "Credits"), 'mcredits');
+	const globalInfo = DataManager.loadGlobalInfo() || []
+	if (globalInfo[0].credits) {
+		this.addCommand(String(Credits.Parameters['按钮标题A'] || '制作组'), 'mcredits_A')
+	} else {
+		this.addCommand(String(Credits.Parameters['按钮标题B'] || '寂しい'), 'mcredits_B')
+	}
+	if (globalInfo[0].memory) {
+		this.addCommand(String(Credits.Parameters['回忆之间组按钮标题'] || '回忆之间'), 'memory');
+	}
 };
 
 
 var _credits = Scene_Title.prototype.createCommandWindow;
 Scene_Title.prototype.createCommandWindow = function () {
 	_credits.call(this);
-	this._commandWindow.setHandler('mcredits', this.commandMCredits.bind(this));
+	this._commandWindow.setHandler('mcredits_A', this.commandCreditsA.bind(this));
+	this._commandWindow.setHandler('mcredits_B', this.commandCreditsB.bind(this));
+	this._commandWindow.setHandler('memory', this.commandMemory.bind(this));
 };
 
-// 公共事件
-Scene_Title.prototype.commandMCredits = function () {
+Scene_Title.prototype.commandCreditsA = function () {
 	DataManager.setupNewGame();
 	this._commandWindow.close();
 	this.fadeOutAll();
 	SceneManager.goto(Scene_Map);
-	$gameTemp.reserveCommonEvent(Number(Credits.Parameters['公共事件ID'] || "Credits"), 1)
+	$gameTemp.reserveCommonEvent(Number(Credits.Parameters['按钮A公共事件ID'] || 1))
+};
+
+Scene_Title.prototype.commandCreditsB = function () {
+	DataManager.setupNewGame();
+	this._commandWindow.close();
+	this.fadeOutAll();
+	SceneManager.goto(Scene_Map);
+	$gameTemp.reserveCommonEvent(Number(Credits.Parameters['按钮B公共事件ID'] || 2))
+};
+
+Scene_Title.prototype.commandMemory = function () {
+	DataManager.setupNewGame();
+	this._commandWindow.close();
+	this.fadeOutAll();
+	SceneManager.goto(Scene_Map);
+	$gameTemp.reserveCommonEvent(Number(Credits.Parameters['回忆之间公共事件ID'] || 3))
+};
+
+var _credits_command = {
+	'SET_MEMOR': 'setMemoryButton',
+	'SET_CREDITS': 'setCreditsButton',
+	'SET_CREDITS_NORMAL': 'setCreditsNormal'
+}
+
+var _credits_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+Game_Interpreter.prototype.pluginCommand = function (command, args) {
+	_credits_pluginCommand.apply(this, arguments);
+	if (_credits_command[command.toUpperCase()]) {
+		$gameSystem[_credits_command[command.toUpperCase()]](JSON.parse(args[0]));
+	}
+};
+
+Game_System.prototype.setMemoryButton = function (flag) {
+	const globalInfo = DataManager.loadGlobalInfo() || [];
+	globalInfo[0] = { ...globalInfo[0], ...{ memory: flag } };
+	DataManager.saveGlobalInfo(globalInfo);
+};
+
+Game_System.prototype.setCreditsButton = function (flag) {
+	const globalInfo = DataManager.loadGlobalInfo() || [];
+	globalInfo[0] = { ...globalInfo[0], ...{ credits: flag } };
+	DataManager.saveGlobalInfo(globalInfo);
+};
+
+Game_System.prototype.setCreditsNormal = function (flag) {
+	const globalInfo = DataManager.loadGlobalInfo() || [];
+	globalInfo[0] = { ...globalInfo[0], ...{ normal: flag } };
+	DataManager.saveGlobalInfo(globalInfo);
 };
