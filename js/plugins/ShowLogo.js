3bfb20ea1bcbef094d841c1e468d38f0ed6316d4
blacktunes <blacktunes@foxmail.com>
2021-05-14 01:55:42 +0800

    perf: 优化LOGO显示插件

--- js/plugins/ShowLogo.js
+++ js/plugins/ShowLogo.js
@@ -1,6 +1,6 @@
 /*:
  * @plugindesc 打开游戏显示LOGO，根据官方插件MadeWithMv修改，增加跳过功能
- * @author Dan "Liquidize" Deptula & Blacktunes
+ * @author Dan 'Liquidize' Deptula & Blacktunes
  *
  * @help This plugin does not provide plugin commands.
  *
@@ -24,28 +24,27 @@
  * @type file
  *
  * @param 渐隐时长
- * Default: 120
  * @default 120
+ * @type Number
  *
  * @param 渐入时长
- * Default: 120
  * @default 120
+ * @type Number
  *
  * @param 显示时长
- * Default: 160
  * @default 160
+ * @type Number
  *
  */
-ShowLogo = {};
+const ShowLogo = {};
 ShowLogo.Parameters = PluginManager.parameters('ShowLogo');
-
-ShowLogo.ShowLogo = JSON.parse(ShowLogo.Parameters["是否显示LOGO图"]);
-ShowLogo.LogoImage = String(ShowLogo.Parameters["LOGO图片"]);
-ShowLogo.ShowCustom = JSON.parse(ShowLogo.Parameters["是否显示自定义图片"]);
-ShowLogo.CustomImage = String(ShowLogo.Parameters["自定义图片"]);
-ShowLogo.FadeOutTime = Number(ShowLogo.Parameters["渐隐时长"]) || 120;
-ShowLogo.FadeInTime = Number(ShowLogo.Parameters["渐入时长"]) || 120;
-ShowLogo.WaitTime = Number(ShowLogo.Parameters["显示时长"]) || 160;
+ShowLogo.ShowLogo = JSON.parse(ShowLogo.Parameters['是否显示LOGO图']);
+ShowLogo.LogoImage = String(ShowLogo.Parameters['LOGO图片']);
+ShowLogo.ShowCustom = JSON.parse(ShowLogo.Parameters['是否显示自定义图片']);
+ShowLogo.CustomImage = String(ShowLogo.Parameters['自定义图片']);
+ShowLogo.FadeOutTime = Number(ShowLogo.Parameters['渐隐时长']) || 120;
+ShowLogo.FadeInTime = Number(ShowLogo.Parameters['渐入时长']) || 120;
+ShowLogo.WaitTime = Number(ShowLogo.Parameters['显示时长']) || 160;
 
 
 //-----------------------------------------------------------------------------
@@ -57,15 +56,15 @@ function Scene_Splash() {
     this.initialize.apply(this, arguments);
 }
 
-(function() {
+(function () {
 
     //-----------------------------------------------------------------------------
     // Scene_Boot
     //
     // The scene class for dealing with the game boot.
-    
+
     var _Scene_Boot_loadSystemImages = Scene_Boot.prototype.loadSystemImages;
-    Scene_Boot.prototype.loadSystemImages = function() {
+    Scene_Boot.prototype.loadSystemImages = function () {
         _Scene_Boot_loadSystemImages.call(this);
         if (ShowLogo.ShowLogo) {
             ImageManager.loadSystem(ShowLogo.LogoImage);
@@ -76,7 +75,7 @@ function Scene_Splash() {
     };
 
     var _Scene_Boot_start = Scene_Boot.prototype.start;
-    Scene_Boot.prototype.start = function() {
+    Scene_Boot.prototype.start = function () {
         if ((ShowLogo.ShowLogo || ShowLogo.ShowCustom) && !DataManager.isBattleTest() && !DataManager.isEventTest()) {
             SceneManager.goto(Scene_Splash);
         } else {
@@ -92,7 +91,7 @@ function Scene_Splash() {
     Scene_Splash.prototype = Object.create(Scene_Base.prototype);
     Scene_Splash.prototype.constructor = Scene_Splash;
 
-    Scene_Splash.prototype.initialize = function() {
+    Scene_Splash.prototype.initialize = function () {
         Scene_Base.prototype.initialize.call(this);
         this._mvSplash = null;
         this._customSplash = null;
@@ -100,39 +99,66 @@ function Scene_Splash() {
         this._customWaitTime = ShowLogo.WaitTime;
         this._mvFadeOut = false;
         this._mvFadeIn = false;
+        this._waitTime = Math.min(ShowLogo.FadeInTime, ShowLogo.FadeOutTime)
         this._customFadeOut = false;
         this._customFadeIn = false;
+        this._skip = false
         if (ShowLogo.ShowLogo || ShowLogo.ShowCustom) {
-            document.addEventListener("keydown", this.skip.bind(this));
-            document.addEventListener("mousedown", this.skip.bind(this));
+            document.addEventListener('keydown', this.skip.bind(this), {
+                once: true
+            });
+            document.addEventListener('mousedown', this.skip.bind(this), {
+                once: true
+            });
         }
     };
 
     Scene_Splash.prototype.skip = function () {
+        if (this._skip) return
+        this._skip = true
         if (ShowLogo.ShowLogo) {
-            if (!this._mvFadeOut) {
+            if (this._mvFadeOut) {
+                this._fadeDuration = 20
+            } else {
                 this._mvFadeOut = true;
-                this.startFadeOut(0, false);
-            } else if (ShowLogo.ShowCustom && this._mvFadeOut && !this._customFadeOut) {
-                this._customFadeOut = true;
-                this._customWaitTime = 0
-                this.startFadeOut(0, false);
+                this.startFadeOut(0, false)
             }
+            if (ShowLogo.ShowCustom) {
+                if (this._waitTime <= 0) {
+                    if (this._customFadeOut) {
+                        this._fadeDuration = 20
+                    } else {
+                        this._customFadeOut = true;
+                        this.startFadeOut(0, false)
+                    }
+                    this._customWaitTime = 0
+                } else {
+                    this._customFadeOut = true;
+                    this._customWaitTime = 0
+                    this.removeChild(this._customSplash)
+                }
+            }
+            this._mvWaitTime = 0
         } else {
-            if (ShowLogo.ShowCustom && !this._customFadeOut) {
-                this._customFadeOut = true
+            if (ShowLogo.ShowCustom) {
+                this.removeChild(this._customSplash)
+                if (this._customFadeOut) {
+                    this._fadeDuration = 20
+                } else {
+                    this._customFadeOut = true;
+                    this.startFadeOut(0, false)
+                }
                 this._customWaitTime = 0
-                this.startFadeOut(0, false);
             }
         }
     }
 
-    Scene_Splash.prototype.create = function() {
+    Scene_Splash.prototype.create = function () {
         Scene_Base.prototype.create.call(this);
         this.createSplashes();
     };
 
-    Scene_Splash.prototype.start = function() {
+    Scene_Splash.prototype.start = function () {
         Scene_Base.prototype.start.call(this);
         SceneManager.clearStack();
         if (this._mvSplash != null) {
@@ -143,7 +169,7 @@ function Scene_Splash() {
         }
     };
 
-    Scene_Splash.prototype.update = function() {
+    Scene_Splash.prototype.update = function () {
         if (ShowLogo.ShowLogo) {
             this._mvSplash.opacity = 255;
             if (!this._mvFadeIn) {
@@ -163,18 +189,21 @@ function Scene_Splash() {
 
         if (ShowLogo.ShowCustom) {
             if (ShowLogo.ShowLogo && this._mvFadeOut == true) {
-                if (!this._customFadeIn && this._fadeDuration == 0) {
-                    this._customSplash.opacity = 255;
-                    this._customWaitTime = ShowLogo.WaitTime;
-                    this.startFadeIn(ShowLogo.FadeInTime, false);
-                    this._customFadeIn = true;
+                if (this._waitTime > 0) {
+                    this._waitTime--
                 } else {
-                    if (this._customWaitTime > 0 && this._customFadeOut == false) {
-                        this._customWaitTime--;
+                    if (!this._customFadeIn && this._fadeDuration == 0) {
+                        this._customSplash.opacity = 255;
+                        this.startFadeIn(ShowLogo.FadeInTime, false);
+                        this._customFadeIn = true;
                     } else {
-                        if (this._customFadeOut == false) {
-                            this._customFadeOut = true;
-                            this.startFadeOut(ShowLogo.FadeOutTime, false);
+                        if (this._customWaitTime > 0 && this._customFadeOut == false) {
+                            this._customWaitTime--;
+                        } else {
+                            if (this._customFadeOut == false) {
+                                this._customFadeOut = true;
+                                this.startFadeOut(ShowLogo.FadeOutTime, false);
+                            }
                         }
                     }
                 }
@@ -211,7 +240,7 @@ function Scene_Splash() {
         Scene_Base.prototype.update.call(this);
     };
 
-    Scene_Splash.prototype.createSplashes = function() {
+    Scene_Splash.prototype.createSplashes = function () {
         if (ShowLogo.ShowLogo) {
             this._mvSplash = new Sprite(ImageManager.loadSystem(ShowLogo.LogoImage));
             this._mvSplash.opacity = 0;
@@ -224,14 +253,14 @@ function Scene_Splash() {
         }
     };
 
-    Scene_Splash.prototype.centerSprite = function(sprite) {
+    Scene_Splash.prototype.centerSprite = function (sprite) {
         sprite.x = Graphics.width / 2;
         sprite.y = Graphics.height / 2;
         sprite.anchor.x = 0.5;
         sprite.anchor.y = 0.5;
     };
 
-    Scene_Splash.prototype.gotoTitleOrTest = function() {
+    Scene_Splash.prototype.gotoTitleOrTest = function () {
         Scene_Base.prototype.start.call(this);
         SoundManager.preloadImportantSounds();
         if (DataManager.isBattleTest()) {
@@ -246,16 +275,14 @@ function Scene_Splash() {
             SceneManager.goto(Scene_Title);
             Window_TitleCommand.initCommandPosition();
         }
-        document.removeEventListener("keydown", this.skip.bind(this));
-        document.removeEventListener("mousedown", this.skip.bind(this));
         this.updateDocumentTitle();
     };
 
-    Scene_Splash.prototype.updateDocumentTitle = function() {
+    Scene_Splash.prototype.updateDocumentTitle = function () {
         document.title = $dataSystem.gameTitle;
     };
 
-    Scene_Splash.prototype.checkPlayerLocation = function() {
+    Scene_Splash.prototype.checkPlayerLocation = function () {
         if ($dataSystem.startMapId === 0) {
             throw new Error('Player\'s starting position is not set');
         }
