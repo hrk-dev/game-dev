81fd2520bdcf229fe56688176b98b75df96a6739
blacktunes <blacktunes@foxmail.com>
2021-06-07 18:01:01 +0800

    perf: 优化远程操控功能

--- src/js/server/socket.js
+++ src/js/server/socket.js
@@ -1,32 +1,34 @@
-var socket = io('http://localhost?k=hrk')
+// 该文件正式版需要修改相应事件和开关ID
+
+var socket = io('http://127.0.0.1?type=game&key=zheshimima')
 
 socket.on('connect', () => {
   console.log('已连接至服务器')
 })
 
-socket.on('open-door', () => {
+socket.on('door', () => {
   if ($gameSwitches) {
-    $gameSwitches.setValue(3, true)
-    console.log('收到开门指令')
-  }
-})
-socket.on('close-door', () => {
-  if ($gameSwitches) {
-    $gameSwitches.setValue(3, false)
-    console.log('收到关门指令')
+    const flag = !!$gameSwitches._data[3]
+    $gameSwitches.setValue(3, !flag)
   }
 })
 
-socket.on('bg-on', () => {
+socket.on('bg', (flag) => {
   if ($gameSwitches) {
-    $gameTemp.reserveCommonEvent(6)
-    console.log('收到改变背景指令')
+    if (flag) {
+      $gameTemp.reserveCommonEvent(7)
+    } else {
+      $gameTemp.reserveCommonEvent(6)
+    }
   }
 })
 
-socket.on('bg-off', () => {
-  if ($gameSwitches) {
-    $gameTemp.reserveCommonEvent(7)
-    console.log('收到恢复背景指令')
+Object.defineProperty(SceneManager, '_scene', {
+  set: (scene) => {
+    SceneManager.__scene = scene
+    socket.emit('set-start', scene.constructor.name === 'Scene_Map')
+  },
+  get: () => {
+    return SceneManager.__scene
   }
 })
